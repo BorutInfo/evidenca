55b01eeca06eab15d88aff5b8ba238b313406afb
BorutInfo <borut.informacije@gmail.com>
2026-06-28 17:16:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,16 @@
-const CACHE_NAME = 'evidenca-v1';
-const ASSETS = [
-  'urnik3.html',
-  'manifest.json'
-];
-
-// Shranjevanje datotek v lokalni pomnilnik ob prvi namestitvi
+// RAZVOJNI NAČIN - IZKLOPLJEN POMNILNIK
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
 });
 
-// Aktivacija in čiščenje starih različic pomnilnika
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map(key => caches.delete(key)));
     })
   );
 });
 
-// Prestrezanje zahtevkov – če ni omrežja, vzemi iz lokalnega pomnilnika
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
